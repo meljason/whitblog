9781410d7d413c5996e0ce63b4b9ed4af0998315
Jason Chong <jasoncwy@hotmail.com>
2020-01-10 01:38:22 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+//this is to include the header
+require 'includes/header.php';
+//this is to include the navigation menu
+    include 'includes/nav.php';
+?>
+
+<?php
+
+
+
+    //this is to include the footer
+    include 'includes/footer.php';
+?>
+
